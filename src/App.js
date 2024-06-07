9163e4f7abb9a554620653f2000d7d24e6c36270
Jayanta66 <jayhalder18@gmail.com>
2024-06-07 12:19:21 +0530

    Success Coching Center 7th-june-2024

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<h1>Success Coching Center</h1>
+<h1>Success Coching Center </h1>
 
       </header>
     </div>
